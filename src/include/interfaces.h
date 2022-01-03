61c61febce023018604a642cfa41e52598b6433a
Dave Gerlach <cdgerlach@gmail.com>
2022-01-02 21:55:31 -0600

    include: interfaces: Introduce ISystem interface
    Introduce an ISystem interface class. Unsurprisingly, his will be used
    for "systems", which are top level blocks of code that will be updated
    every cycle of the game engine. Examples of systems are physics and
    rendering, which are both special cases, but generic systems will also
    be made available as well that can be added and will tick along with
    everything else.

--- src/include/interfaces.h
+++ src/include/interfaces.h
@@ -2,6 +2,7 @@
 #define _INTERFACES_H_
 
 #include <memory>
+#include <string>
 
 class IEventData
 {
@@ -9,6 +10,12 @@ public:
 	virtual const unsigned long & getType(void) const = 0;
 	virtual std::shared_ptr<IEventData> copy(void) const = 0;
 	virtual const char* getName(void) const = 0;
+};
+
+class ISystem {
+public:
+	virtual void onUpdate(double dt) = 0;
+	virtual std::string getName(void) = 0;
 };
 
 #endif /* _INTERFACES_H_ */
